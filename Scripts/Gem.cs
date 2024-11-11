c558d216ee031fc4769f8a0a5b2a33f82d29327a
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-11-11 15:03:51 -0700

    Update Gem.cs

--- Scripts/Gem.cs
+++ Scripts/Gem.cs
@@ -9,19 +9,10 @@ public class Gem : MonoBehaviour
         gemManager = FindObjectOfType<GemManager>();
     }
 
-    void Update()
-    {
-        // Prevent any unintended rotation
-        transform.rotation = Quaternion.identity;
-    }
-
-    void OnMouseOver()
+    void OnMouseDown()
     {
         // Collect the gem on left-click
-        if (Input.GetMouseButtonDown(0))
-        {
-            gemManager.CollectGem();
-            Destroy(gameObject);  // Remove the gem from the scene
-        }
+        gemManager.CollectGem();
+        Destroy(gameObject);  // Remove the gem from the scene
     }
 }
